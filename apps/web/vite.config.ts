4a3bb3f3630aa0ec848c57b2e647693d7151c9c4
SalemAttia <salem.at.ibrahim@gmail.com>
2025-08-03 16:06:11 +0200

    fix

--- apps/web/vite.config.ts
+++ apps/web/vite.config.ts
@@ -3,6 +3,7 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import { nxViteTsPaths } from '@nx/vite/plugins/nx-tsconfig-paths.plugin';
 import { nxCopyAssetsPlugin } from '@nx/vite/plugins/nx-copy-assets.plugin';
+import { join } from 'path';
 
 export default defineConfig(() => ({
   root: __dirname,
@@ -26,6 +27,11 @@ export default defineConfig(() => ({
     reportCompressedSize: true,
     commonjsOptions: {
       transformMixedEsModules: true,
+    },
+    rollupOptions: {
+      input: {
+        main: join(__dirname, 'index.html'),
+      },
     },
   },
   test: {
