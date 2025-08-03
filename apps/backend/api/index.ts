d59f2126241b0ae6f4e466ce2ce99b78a2ec37db
SalemAttia <salem.at.ibrahim@gmail.com>
2025-08-03 19:44:24 +0200

    fix

---
+++ apps/backend/api/index.ts
@@ -0,0 +1,10 @@
+import express from "express";
+const app = express();
+
+app.get("/api", (req, res) => {
+  res.setHeader('Content-Type', 'text/html');
+  res.setHeader('Cache-Control', 's-max-age=1, stale-while-revalidate');
+  res.send("Hello from Vooksio API!");
+});
+
+module.exports = app;
