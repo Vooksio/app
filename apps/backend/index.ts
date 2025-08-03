3e5035b6caad744815df6d7ee1a39960c100b2ff
SalemAttia <salem.at.ibrahim@gmail.com>
2025-08-03 19:33:12 +0200

    fix

--- apps/backend/index.ts
+++ apps/backend/index.ts
@@ -21,12 +21,9 @@ app.use((err: Error, req: Request, res: Response, next: Function) => {
   next();
 });
 
-// For local development
-if (process.env.NODE_ENV !== 'production') {
-  const port = process.env.PORT || 3000;
-  app.listen(port, () => {
-    console.log(`⚡️[server]: Server is running at http://localhost:${port}`);
-  });
-}
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+  console.log(`⚡️[server]: Server is running on port ${port}`);
+});
 
 export default app;
